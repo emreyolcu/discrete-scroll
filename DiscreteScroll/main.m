4bfdc6bd590a320706632b80fa88d6db932b671e
Emre Yolcu <emreyolcu@live.com>
2016-11-25 13:00:44 +0200

    Initial commit

---
+++ DiscreteScroll/main.m
@@ -0,0 +1,35 @@
+#import <ApplicationServices/ApplicationServices.h>
+
+#define SIGN(x) (((x) > 0) - ((x) < 0))
+#define LINES 3
+
+CGEventRef cgEventCallback(CGEventTapProxy proxy, CGEventType type,
+                           CGEventRef event, void *refcon)
+{
+    if (!CGEventGetIntegerValueField(event, kCGScrollWheelEventIsContinuous)) {
+        int64_t delta = CGEventGetIntegerValueField(event, kCGScrollWheelEventDeltaAxis1);
+        
+        CGEventSetIntegerValueField(event, kCGScrollWheelEventDeltaAxis1, SIGN(delta) * LINES);
+    }
+    
+    return event;
+}
+
+int main(void)
+{
+    CFMachPortRef eventTap;
+    CFRunLoopSourceRef runLoopSource;
+    
+    eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0,
+                                1 << kCGEventScrollWheel, cgEventCallback, NULL);
+    runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
+    
+    CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
+    CGEventTapEnable(eventTap, true);
+    CFRunLoopRun();
+    
+    CFRelease(eventTap);
+    CFRelease(runLoopSource);
+    
+    return 0;
+}
