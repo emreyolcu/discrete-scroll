8c4b5149b4c14fb13512db5db79d5f15c8c2c2c4
Ole Scheller <olescheller87@gmail.com>
2017-10-22 20:36:25 +0200

    Add faster scrolling for VERY fast mouse wheel movement

--- DiscreteScroll/main.m
+++ DiscreteScroll/main.m
@@ -2,14 +2,22 @@
 
 #define SIGN(x) (((x) > 0) - ((x) < 0))
 #define LINES 3
+#define QUICK_SCROLL_LIMITATION 100
+#define QUICK_SCROLL_LINES 12
 
 CGEventRef cgEventCallback(CGEventTapProxy proxy, CGEventType type,
                            CGEventRef event, void *refcon)
 {
     if (!CGEventGetIntegerValueField(event, kCGScrollWheelEventIsContinuous)) {
         int64_t delta = CGEventGetIntegerValueField(event, kCGScrollWheelEventPointDeltaAxis1);
-        
-        CGEventSetIntegerValueField(event, kCGScrollWheelEventDeltaAxis1, SIGN(delta) * LINES);
+        int64_t lines_to_scroll;
+
+        if (delta < QUICK_SCROLL_LIMITATION && delta > QUICK_SCROLL_LIMITATION * -1) {
+            lines_to_scroll = SIGN(delta) * LINES;
+        } else {
+            lines_to_scroll = SIGN(delta) * QUICK_SCROLL_LINES;
+        }
+        CGEventSetIntegerValueField(event, kCGScrollWheelEventDeltaAxis1, lines_to_scroll);
     }
     
     return event;
