5ff02128008c1d81b4970787130432d56e16ec89
Emre Yolcu <yolcu.emre@outlook.com>
2021-10-12 15:45:05 -0400

    Clean up source

--- DiscreteScroll/main.m
+++ DiscreteScroll/main.m
@@ -3,15 +3,14 @@
 #define SIGN(x) (((x) > 0) - ((x) < 0))
 #define LINES 3
 
-CGEventRef cgEventCallback(CGEventTapProxy proxy, CGEventType type,
-                           CGEventRef event, void *refcon)
+CGEventRef cgEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef event, void *refcon)
 {
     if (!CGEventGetIntegerValueField(event, kCGScrollWheelEventIsContinuous)) {
         int64_t delta = CGEventGetIntegerValueField(event, kCGScrollWheelEventPointDeltaAxis1);
-        
+
         CGEventSetIntegerValueField(event, kCGScrollWheelEventDeltaAxis1, SIGN(delta) * LINES);
     }
-    
+
     return event;
 }
 
@@ -19,17 +18,16 @@ int main(void)
 {
     CFMachPortRef eventTap;
     CFRunLoopSourceRef runLoopSource;
-    
-    eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0,
-                                1 << kCGEventScrollWheel, cgEventCallback, NULL);
+
+    eventTap = CGEventTapCreate(kCGSessionEventTap, kCGHeadInsertEventTap, 0, 1 << kCGEventScrollWheel, cgEventCallback, NULL);
     runLoopSource = CFMachPortCreateRunLoopSource(kCFAllocatorDefault, eventTap, 0);
-    
+
     CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
     CGEventTapEnable(eventTap, true);
     CFRunLoopRun();
-    
+
     CFRelease(eventTap);
     CFRelease(runLoopSource);
-    
+
     return 0;
 }
